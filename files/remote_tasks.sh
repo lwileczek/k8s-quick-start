0a0b692e79e4cabb69233a953cf7407e9a5cb0a4
lwileczek <wileczek105@gmail.com>
2020-04-06 16:53:12 -0400

    issue 1 & 4/5 of issue 2, create inventory & choose kubespray branch

--- files/remote_tasks.sh
+++ files/remote_tasks.sh
@@ -1,26 +1,11 @@
 #!/bin/bash
 
-chdir ~/kubespray
-
-declare -a IPS=( )  # add in the ips here for each node in your cluster
-CONFIG_FILE=inventory/mycluster/hosts.yml python3 contrib/inventory_builder/inventory.py ${IPS[@]}
-
-# change deploy user
-echo '## User defined user on remote hosts
-ansible_user: {{ deploy_user }}
-ansible_connection: ssh
-ansible_python_interpreter: /usr/bin/python3' >> inventory/mycluster/group_vars/all/all.yml
 
 # deploy cluster
 ansible-playbook -i inventory/mycluster/hosts.yml --become --become-user=root cluster.yml
 
 # setup kubectl
 chdir ~/
-
-curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
-
-chmod +x ./kubectl
-mv ./kubectl /usr/local/bin/kubectl
 
 # change master_node_ip to
 ssh {{ master_node_ip }} sudo cp /etc/kubernetes/admin.conf /home/{{ deploy_user }}/config
