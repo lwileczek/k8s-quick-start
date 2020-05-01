68f08924e3d514b4897aded4713ec90174fb2821
lwileczek <wileczek105@gmail.com>
2020-05-01 14:55:42 -0400

    init lxc provisioning

---
+++ provisioning/lxc/bootstrap-lxc-ubuntu.sh
@@ -0,0 +1,43 @@
+#/bin/bash
+#
+# bootstrap-lxc.sh
+# Setup a kubernetes cluster inside of LXC Containers on a linux system. Default
+# is to set up three nodes, one master and two worker nodes.
+#
+# Assumptions:
+# - The following packages are installed
+#   - LXC
+#   - LXD
+#   - cURL
+# - OS is Linux
+# - Being deployed from the same folder as
+#   - profile.yaml
+#   - lxd_init.yaml
+#   - bootstrap-kube-centos.sh
+# - Running as root
+#
+# Invoke:
+# ./boostrap-lxc.sh
+
+cat ldx_init.yaml | lxd init --preseed  # take our file to start lxd
+
+lxc profile create k8s-profile
+lxc profile edit k8s-profile < k8s-profile.yaml
+
+# Launch three CentOS containers
+for node in kmaster kworker0 kworker1
+do
+    lxc launch images:centos/7 ${node} --profile k8s-profile
+    cat bootstrap-kube-centos.sh | lxc exec ${node} bash
+done
+
+# Platform agnositc kubectl download
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+# To download a specific version you can use the below replacing `v1.18.0` with
+# the desired version.
+#  curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.18.0/bin/linux/amd64/kubectl
+
+chmod +x ./kubectl
+mv ./kubectl /usr/local/bin/kubectl
+mkdir ~/.kube
+
